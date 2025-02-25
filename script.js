e6b938270ddbc7a3e17404b517b662564f163458
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-25 11:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function firstWord(s) {
   // your code here
-	if(s.length == 0){
-		return '';
-	}
-	s.trim();
-	let arr = s.split(' ');
-	return arr[0];
-	
+  if (s.length === 0) {
+    return '';
+  }
+  s = s.trim(); // Reassign the trimmed value back to s
+  let arr = s.split(' ');
+  return arr[0];
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
